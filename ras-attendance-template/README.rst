dae87bf2499fe98a845b250604417b089b1f6004
lubusax <lu.bu.sax@google.com>
2021-12-21 21:19:45 +0100

    init

---
+++ ras-attendance-template/README.rst
@@ -0,0 +1,19 @@
+=================================================
+Template to configure new RFID Attendance Devices
+=================================================
+
+This template delivers the configuration needed to define new RAS2 devices on the OCA-IoT Modules.
+
+
+Credits
+=======
+
+Authors
+~~~~~~~
+
+* thingsintouch.com
+
+Maintainers
+~~~~~~~~~~~
+
+This module is maintained by thingsintouch.com
---
+++ ras-attendance-template/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2018 Creu Blanca
+# Copyright (C) 2021 thingsintouch.com
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "RAS Template for IoT/OCA",
+    "version": "13.0.1.0.0",
+    "category": "IoT",
+    "author": "Creu Blanca , "
+            "thingsintouch.com",
+    "website": "https://github.com/thingsintouch/ras-template",
+    "license": "AGPL-3",
+    "installable": True,
+    "summary": "Template to define new RAS Devices for OCA-IoT Modules",
+    "depends": ["hr_attendance_rfid", "iot_option_oca"],
+    "data": ["data/ras_template.xml"],
+}

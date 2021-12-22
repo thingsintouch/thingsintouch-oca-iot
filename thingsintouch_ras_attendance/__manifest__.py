1f31c6affa61326c67f0c2b2f2bfabecb610b66e
lubusax <lu.bu.sax@google.com>
2021-12-22 10:35:06 +0100

    Naming, Description and Website

--- thingsintouch_ras_attendance/__manifest__.py
+++ thingsintouch_ras_attendance/__manifest__.py
@@ -2,15 +2,15 @@
 # Copyright (C) 2021 thingsintouch.com
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "RAS Template for IoT/OCA",
+    "name": "RAS Configuration for IoT/OCA",
     "version": "14.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca , "
             "thingsintouch.com",
-    "website": "https://github.com/thingsintouch/thingsintouch-oca-iot/tree/13.0/ras-attendance-template",
+    "website": "https://github.com/thingsintouch/iot-devices",
     "license": "AGPL-3",
     "installable": True,
-    "summary": "Template to define new RAS Devices for OCA-IoT Modules",
+    "summary": "Template and Functionality to define new RAS Devices for OCA-IoT Modules",
     "depends": ["hr_attendance_rfid", "iot_option_oca"],
     "data": ["data/ras_template.xml"],
 }

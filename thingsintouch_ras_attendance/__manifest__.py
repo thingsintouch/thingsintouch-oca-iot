5dca44baabe3cafb1227174812d30dd90697a525
lubusax <lu.bu.sax@gmail.com>
2022-02-02 19:50:14 +0100

    [12.0][MIG]manifest

--- thingsintouch_ras_attendance/__manifest__.py
+++ thingsintouch_ras_attendance/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "RAS Configuration for IoT/OCA",
-    "version": "13.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca , "
             "thingsintouch.com",

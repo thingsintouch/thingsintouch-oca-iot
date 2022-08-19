63ad6d39afa9fc14b9a7e029f0e6b54507f83056
lubusax <lu.bu.sax@gmail.com>
2022-08-19 14:20:37 +0000

    [ADD] 12.0 ras_simplified template

---
+++ thingsintouch_ras_simplified/models/hr_employee.py
@@ -0,0 +1,20 @@
+# Copyright 2022 thingsintouch.com
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, models
+
+from datetime import datetime
+
+import freezegun
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    @api.model
+    def register_attendance_async(self, card_code, timestamp):
+        with freezegun.freeze_time(datetime.fromtimestamp(int(timestamp), tz=None)):
+            result = self.register_attendance(card_code)
+        return result
+
+

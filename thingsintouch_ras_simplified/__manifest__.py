2d89827da7133688ad98a505f130eb26dd1acabd
lubusax <lu.bu.sax@gmail.com>
2022-08-19 15:00:56 +0000

    [ADD] ras_simplified template

--- thingsintouch_ras_simplified/__manifest__.py
+++ thingsintouch_ras_simplified/__manifest__.py
@@ -2,15 +2,15 @@
 # Copyright (C) 2022 thingsintouch.com
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "RAS Configuration for IoT/OCA",
-    "version": "14.0.2.0.0",
+    "name": "RAS Simplified Configuration for IoT/OCA",
+    "version": "14.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca , "
             "thingsintouch.com",
     "website": "https://github.com/thingsintouch/iot-devices",
     "license": "AGPL-3",
     "installable": True,
-    "summary": "Template and Functionality to define new RAS Devices for OCA-IoT Modules",
-    "depends": ["thingsintouch_ras_simplified", "iot_option_oca"],
-    "data": ["data/ras_template.xml"],
+    "summary": "Simplified Template and Functionality to define new RAS Devices for OCA-IoT Modules",
+    "depends": ["hr_attendance_rfid", "iot_template_oca"],
+    "data": ["data/ras_simplified_template.xml"],
 }

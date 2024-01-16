c287ed3a3745cb564588448eb7bbbf7ef9879e23
lubusax <lu.bu.sax@gmail.com>
2024-01-16 10:25:20 +0000

    [16.0][MIG] ras_simplified migrated to 16.0

--- thingsintouch_ras_simplified/__manifest__.py
+++ thingsintouch_ras_simplified/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2018 Creu Blanca
-# Copyright (C) 2022 thingsintouch.com
+# Copyright (C) 2024 thingsintouch.com
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "RAS Simplified Configuration for IoT/OCA",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "IoT",
     "author": "Creu Blanca , "
             "thingsintouch.com",
